0027a2b263f8a7c71f98320b8974b041331a6b0a
Frisayl <45555761+Frisayl@users.noreply.github.com>
2019-06-03 12:53:10 +0200

    yo

--- paskuisland/core/src/sgs/entities/Entity.java
+++ paskuisland/core/src/sgs/entities/Entity.java
@@ -5,9 +5,14 @@ import com.badlogic.gdx.math.Vector2;
 
 public abstract class Entity {
 	
+	
+	
 	public Vector2 position;
 
 	public Entity() {
+	
+		 
+		
 		// TODO Auto-generated constructor stub
 	}
 	
--- paskuisland/core/src/sgs/entities/Omino.java
+++ paskuisland/core/src/sgs/entities/Omino.java
@@ -4,18 +4,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import java.util.Random;
 
 public class Omino extends Entity {
 	
 	public static Texture texture = new Texture(Gdx.files.internal("spy.png"));
-
-	public Omino(float x, float y) {
+	  
+	  public float strength; 
+	  public float speed;
+	  public float hunger; 
+	  public float sociality;
+      
+	  public Omino(float x, float y, float strength, float speed, float hunger, float sociality) {
 		super();
 		super.position = new Vector2(x,y);
-	}
+		float S = new Random().nextFloat();
+		this.strength = S;
+		float So = new Random().nextFloat();
+		this.sociality = So;
+		float Sp = new Random().nextFloat();
+		this.speed = 2*So;
+		this.hunger = 0;
+		}
 	
 	public void disegnami(SpriteBatch batch) {
 		batch.draw(texture, position.x, position.y);
 	}
+	public void cheDevofa{
+		 
+		
+		
+		}
+    public void cheStamoAFa {
+    	
+    	
+    }
+    
 
 }
+ 
